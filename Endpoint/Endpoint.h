d2b292162c5486ad822f072a353416a4c2418536
Nate Stedman <natesm@gmail.com>
2016-01-20 22:17:36 -0500

    Add initial project files.

---
+++ Endpoint/Endpoint.h
@@ -0,0 +1,17 @@
+// Endpoint
+// Written in 2016 by Nate Stedman <nate@natestedman.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Endpoint.
+FOUNDATION_EXPORT double EndpointVersionNumber;
+
+//! Project version string for Endpoint.
+FOUNDATION_EXPORT const unsigned char EndpointVersionString[];
